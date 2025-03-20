f6c41a309e21410a94adb3bed74096257ecbdffd
ujjwal mittal <ujjwalmittal012@gmail.com>
2025-03-20 23:48:41 +0530

    Temporary rename to fix case sensitivity

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
-import Services from './pages/services';
+import Services from "./pages/Services";
 import Equity from './pages/products/Equity';
 import Derivatives from './pages/products/Derivatives';
 import Commodities from './pages/products/Commodities';
